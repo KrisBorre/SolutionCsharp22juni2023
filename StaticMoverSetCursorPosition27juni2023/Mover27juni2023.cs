836905d9e63e5ee4a81289887577adc7e5f634ef
kris_borre <kris_borre@DESKTOP-V0LQE95>
2023-06-27 16:05:33 +0200

    SetWindowSize

---
+++ StaticMoverSetCursorPosition27juni2023/Mover27juni2023.cs
@@ -0,0 +1,63 @@
+﻿namespace StaticMoverSetCursorPosition27juni2023
+{
+    internal class Mover27juni2023
+    {
+        public Mover27juni2023(int xi, int yi, int dxi, int dyi)
+        {
+            x = xi;
+            y = yi;
+            dx = dxi;
+            dy = dyi;
+        }
+
+        private static int height;
+        public static int Height
+        {
+            get { return height; }
+            set
+            {
+                if (value > 0 && value < Console.LargestWindowHeight)
+                    height = value;
+            }
+        }
+
+        private static int width;
+        public static int Width
+        {
+            get { return width; }
+            set
+            {
+                if (value > 0 && value < Console.LargestWindowWidth)
+                    width = value;
+            }
+        }
+
+        private int dx = 1;
+        private int dy = 0;
+        private int x = 0;
+        private int y = 0;
+
+        public void Update()
+        {
+            x += dx;
+            if (x >= Width || x < 0) 
+            {
+                dx *= -1;
+                x += dx;
+            }
+
+            y += dy;
+            if (y >= Height || y < 0)
+            {
+                dy *= -1;
+                y += dy;
+            }
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write("O");
+        }
+    }
+}
---
+++ StaticMoverSetCursorPosition27juni2023/Program.cs
@@ -0,0 +1,39 @@
+﻿using StaticMoverSetCursorPosition27juni2023;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        #region static
+        {            
+            Console.CursorVisible = false; 
+            Mover27juni2023.Height = Console.WindowHeight;
+            Mover27juni2023.Width = Console.WindowWidth;
+
+            Mover27juni2023 m1 = new Mover27juni2023(1, 1, 1, 1);
+            Mover27juni2023 m2 = new Mover27juni2023(6, 7, -2, 1);
+
+            for (int count = 0; count < 1_000_000; count++)
+            {
+                if (count % 20 == 0) // iedere seconde (daar we telkens 50ms slapen (1 seconde = 1000 ms => 1000ms/50ms == 20))
+                {
+                    Mover27juni2023.Width++;
+                    Mover27juni2023.Height++;
+                }
+                Console.SetWindowSize(Mover27juni2023.Width, Mover27juni2023.Height);
+
+                m1.Update();
+                m1.Draw();
+
+                m2.Update();
+                m2.Draw();
+
+                Thread.Sleep(50);
+                Console.Clear();
+            }
+
+            Console.WriteLine("Kris Borre");
+        }
+        #endregion
+    }
+}
